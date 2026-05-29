1ee0c012f4f6023be3aafab5610f2ca9e7eed699
Chu Thế Văn <chuthevan450@gmail.com>
2026-05-29 14:47:51 +0700

    Update api.js

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend base URL
+  baseURL: process.env.VITE_API_URL, // Backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
